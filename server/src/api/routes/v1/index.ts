8d9af43c41f93997bf602421c9d8024fbab5371b
dajneem23 <dajneem23@gmail.com>
2022-10-16 21:13:47 +0700

    feat: add healthy

--- server/src/api/routes/v1/index.ts
+++ server/src/api/routes/v1/index.ts
@@ -12,6 +12,10 @@ const route = Router();
 export default (app: Application) => {
   app.use(`${env.API_PREFIX}/v1`, route);
 
+  app.use('/health', (req, res) => {
+    res.status(200).json({ status: 'ok' });
+  });
+
   attachControllers(route, [UserController, AuthController]);
 
   attachControllers(route, [FilmController, NewsController, MangaController, StoryController]);
