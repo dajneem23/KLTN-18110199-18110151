74d03c5d59734daa4f70d0f95405044df829fb23
dajneem23 <dajneem23@gmail.com>
2022-11-20 21:15:41 +0700

    feat: admin

--- admin/api/article/controllers/article.js
+++ admin/api/article/controllers/article.js
@@ -1,16 +1,10 @@
-const { sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
-  /**
-   * Retrieve a record.
-   *
-   * @return {Object}
-   */
+	/**
+	 * Retrieve a record.
+	 *
+	 * @return {Object}
+	 */
 
-  async findOne(ctx) {
-    const { slug } = ctx.params;
-
-    const entity = await strapi.services.article.findOne({ slug });
-    return sanitizeEntity(entity, { model: strapi.models.article });
-  },
 };
