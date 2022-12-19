47d59b68c61b8a718cb92bb41236f5a22f69e035
dajneem23 <dajneem23@gmail.com>
2022-12-19 14:20:09 +0700

    feat: strapi::body size

--- admin/config/middlewares.js
+++ admin/config/middlewares.js
@@ -14,11 +14,21 @@ module.exports = [
 			},
 		},
 	},
+	{
+		name: "strapi::body",
+		config: {
+			formLimit: "512mb", // modify form body
+			jsonLimit: "512mb", // modify JSON body
+			textLimit: "512mb", // modify text body
+			formidable: {
+				maxFileSize: 500 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+			},
+		},
+	},
 	"strapi::cors",
 	"strapi::poweredBy",
 	"strapi::logger",
 	"strapi::query",
-	"strapi::body",
 	"strapi::favicon",
 	"strapi::public",
 ];
